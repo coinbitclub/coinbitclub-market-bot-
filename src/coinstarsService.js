4f89292f8abeb78d8b0856d16a1343e1e8152cf1
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-11 20:49:34 -0300

    Update coinstarsService.js

--- src/coinstarsService.js
+++ src/coinstarsService.js
@@ -1,20 +1,16 @@
 import axios from 'axios';
-import { Pool } from 'pg';
 
 const COINSTATS_API_KEY = process.env.COINSTATS_API_KEY;
 const BASE_URL = "https://openapiv1.coinstats.app/insights";
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-// Fear & Greed
-export async function getFearGreedIndexAndSave() {
+export async function getFearGreedIndexAndSave(pool) {
   try {
     const res = await axios.get(`${BASE_URL}/fear-and-greed`, {
       headers: { 'X-API-KEY': COINSTATS_API_KEY }
     });
-    // Salva na tabela coinstats_fear_greed (received_at, raw_payload)
     await pool.query(
-      'INSERT INTO coinstats_fear_greed (received_at, raw_payload) VALUES (NOW(), $1)',
-      [JSON.stringify(res.data)]
+      'INSERT INTO coinstats_fear_greed (raw_payload) VALUES ($1)',
+      [res.data]
     );
     return res.data;
   } catch (err) {
@@ -23,16 +19,14 @@ export async function getFearGreedIndexAndSave() {
   }
 }
 
-// BTC Dominance
-export async function getBTCDominanceAndSave() {
+export async function getBTCDominanceAndSave(pool) {
   try {
     const res = await axios.get(`${BASE_URL}/btc-dominance`, {
       headers: { 'X-API-KEY': COINSTATS_API_KEY }
     });
-    // Salva na tabela coinstats_btc_dominance (received_at, raw_payload)
     await pool.query(
-      'INSERT INTO coinstats_btc_dominance (received_at, raw_payload) VALUES (NOW(), $1)',
-      [JSON.stringify(res.data)]
+      'INSERT INTO coinstats_btc_dominance (raw_payload) VALUES ($1)',
+      [res.data]
     );
     return res.data;
   } catch (err) {

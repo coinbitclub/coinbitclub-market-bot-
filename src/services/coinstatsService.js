16536f42cbaa630dcd9ab395dc9a3358a4d4ace4
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-12 15:18:42 -0300

    Update coinstatsService.js

--- src/services/coinstatsService.js
+++ src/services/coinstatsService.js
@@ -1,25 +1,20 @@
 import axios from 'axios';
-import { save } from './databaseService.js';
+import {
+  saveFearGreed,
+  saveBTCDominance
+} from './databaseService.js';
 
-const URL_FG   = 'https://openapiv1.coinstats.app/insights/fear-and-greed';
-const URL_DOM  = 'https://openapiv1.coinstats.app/insights/btc-dominance';
+const FG_URL = 'https://openapiv1.coinstats.app/insights/fear-and-greed';
+const DOM_URL = 'https://openapiv1.coinstats.app/insights/btc_dominance';
 
-export async function getFearAndGreedAndSave() {
-  try {
-    const { data } = await axios.get(URL_FG);
-    await save('coinstats_fear_greed', JSON.stringify(data));
-    console.log('[CoinStats] Fear & Greed salvo!');
-  } catch (err) {
-    console.error('[CoinStats] Cron Fear & Greed:', err.message);
-  }
+export async function fetchAndSaveFearGreed() {
+  const { data } = await axios.get(FG_URL);
+  await saveFearGreed(data);
+  return data;
 }
 
-export async function getBTCDominanceAndSave() {
-  try {
-    const { data } = await axios.get(URL_DOM);
-    await save('coinstats_btc_dominance', JSON.stringify(data));
-    console.log('[CoinStats] BTC Dominance salvo!');
-  } catch (err) {
-    console.error('[CoinStats] Cron BTC Dominance:', err.message);
-  }
+export async function fetchAndSaveBTCDominance() {
+  const { data } = await axios.get(DOM_URL);
+  await saveBTCDominance(data);
+  return data;
 }

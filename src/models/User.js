ef4ee68b79db13ee81c32b79335abb1a1358a48b
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-12 18:22:14 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,9 @@
-// models/User.js
-import { query } from '../services/databaseService.js'
-
-/**
- * Insere (ou atualiza) um usuário com suas credenciais Bybit criptografadas.
- */
-export async function upsertUser({ userId, nickname, apiKeyEnc, apiSecretEnc }) {
-  const sql = `
-    INSERT INTO users (id, nickname, bybit_api_key, bybit_api_secret)
-    VALUES ($1,$2,$3,$4)
-    ON CONFLICT (id) DO UPDATE
-      SET nickname = EXCLUDED.nickname,
-          bybit_api_key = EXCLUDED.bybit_api_key,
-          bybit_api_secret = EXCLUDED.bybit_api_secret
-    RETURNING *
-  `
-  const values = [userId, nickname, apiKeyEnc, apiSecretEnc]
-  const res = await query(sql, values)
-  return res.rows[0]
-}
-
-/** Busca usuário pelo ID */
-export async function getUserById(userId) {
-  const res = await query(`SELECT * FROM users WHERE id = $1`, [userId])
-  return res.rows[0] || null
+export default class User {
+  constructor(obj) {
+    this.id = obj.id;
+    this.bybitApiKey = obj.bybit_api_key;
+    this.bybitApiSecret = obj.bybit_api_secret;
+    this.capitalPct = obj.capital_per_order;
+    this.leverage = obj.leverage;
+  }
 }

92b8f5ff0e2cfe84ebfb1e6eee7359a2255df44f
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-12 18:21:26 -0300

    Update Signal.js

--- src/models/Signal.js
+++ src/models/Signal.js
@@ -1,22 +1,6 @@
-// models/Signal.js
-import { query } from '../services/databaseService.js'
-
-/** Registra um sinal de setup vindo do TradingView */
-export async function createSignal({ userId, rawPayload }) {
-  const sql = `
-    INSERT INTO signals (user_id, raw_payload)
-    VALUES ($1, $2)
-    RETURNING *
-  `
-  const res = await query(sql, [userId, rawPayload])
-  return res.rows[0]
-}
-
-/** Lista últimos N sinais */
-export async function listRecentSignals(limit = 50) {
-  const res = await query(
-    `SELECT * FROM signals ORDER BY received_at DESC LIMIT $1`,
-    [limit]
-  )
-  return res.rows
+export default class Signal {
+  constructor(obj) {
+    this.time = obj.received_at;
+    this.payload = obj.raw_payload;
+  }
 }

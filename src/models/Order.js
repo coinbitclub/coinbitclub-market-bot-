12109b0f5a56fc958afb8e0e55f736114d3cdd69
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-12 18:21:02 -0300

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,41 +1,10 @@
-// models/Order.js
-import { query } from '../services/databaseService.js'
-
-/** Registra ordem que abrimos na Bybit */
-export async function createOrder({
-  userId,
-  symbol,
-  direction,
-  entryPrice,
-  leverage,
-  rawResponse
-}) {
-  const sql = `
-    INSERT INTO orders
-      (user_id, symbol, direction, entry_price, leverage, raw_payload)
-    VALUES ($1,$2,$3,$4,$5,$6)
-    RETURNING *
-  `
-  const res = await query(sql, [
-    userId,
-    symbol,
-    direction,
-    entryPrice,
-    leverage,
-    rawResponse
-  ])
-  return res.rows[0]
-}
-
-/** Atualiza ordem ao fechar (TP/SL ou cancelamento) */
-export async function closeOrder({ orderId, exitPrice, rawResponse }) {
-  const sql = `
-    UPDATE orders
-      SET exit_price = $2,
-          raw_payload = raw_payload || $3::jsonb
-    WHERE id = $1
-    RETURNING *
-  `
-  const res = await query(sql, [orderId, exitPrice, rawResponse])
-  return res.rows[0]
+export default class Order {
+  constructor(obj) {
+    this.id = obj.id;
+    this.userId = obj.user_id;
+    this.direction = obj.direction;
+    this.entry = obj.entry;
+    this.exit = obj.exit;
+    this.result = obj.result;
+  }
 }

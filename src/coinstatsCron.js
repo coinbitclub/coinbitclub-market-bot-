a77f01b4a5eaa6fedbe721f2311d7afaf53c33b9
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-12 16:24:08 -0300

    Update coinstatsCron.js

--- src/coinstatsCron.js
+++ src/coinstatsCron.js
@@ -1,24 +1,13 @@
+// src/coinstatsCron.js
 import cron from 'node-cron';
 import {
-  fetchAndSaveFearGreed,
-  fetchAndSaveBTCDominance
-} from './coinstatsService.js';
+  getBTCDominanceAndSave,
+  getFearAndGreedAndSave
+} from './services/coinstatsService.js';
 
-export function startCoinstatsCron() {
-  // a cada 30 minutos
-  cron.schedule('0,30 * * * *', async () => {
-    try {
-      await fetchAndSaveBTCDominance();
-      console.log('[Cron] BTC Dominance salvo');
-    } catch (err) {
-      console.error('[Cron BTC] erro:', err.message);
-    }
+cron.schedule('0,30 * * * *', () => {
+  getBTCDominanceAndSave().catch(e => console.error('[CoinStats] Cron BTC Dominance:', e));
+  getFearAndGreedAndSave().catch(e => console.error('[CoinStats] Cron Fear & Greed:', e));
+});
 
-    try {
-      await fetchAndSaveFearGreed();
-      console.log('[Cron] Fear & Greed salvo');
-    } catch (err) {
-      console.error('[Cron FG] erro:', err.message);
-    }
-  }, { timezone: 'UTC' });
-}
+export default cron;

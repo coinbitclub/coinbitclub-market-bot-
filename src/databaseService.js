e657cf6939a8b8f3a783d51b5bd9d941979d4a92
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 03:49:43 -0300

    Update databaseService.js

--- src/databaseService.js
+++ src/databaseService.js
@@ -1,19 +1,4 @@
-/* =============================================
-   src/databaseService.js
-   ============================================= */
 import pkg from 'pg';
-import { logger } from './logger.js';
 const { Pool } = pkg;
-
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export async function query(text, params) {
-  const start = Date.now();
-  try {
-    const res = await pool.query(text, params);
-    logger.info(`DB QUERY ${text} [${Date.now()-start}ms]`);
-    return res;
-  } catch (err) {
-    logger.error(`DB ERROR ${err.message}`);
-    throw err;
-  }
-}
+export function query(text, params) { return pool.query(text, params); }

49d4944d968529b0c3015bbcca0a8d794f45e092
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 04:52:57 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webhookRoutes from './webhooks.js';
-import apiRoutes from './webhooks.js';
 import { setupScheduler } from './utils/scheduler.js';
 import { logger } from './logger.js';
 
 dotenv.config();
 const app = express();
-
 app.use(express.json({ limit: '100kb' }));
+
+// Webhooks de sinal e dominance
 app.use('/webhook', webhookRoutes);
-app.use('/api', apiRoutes);
+// Health check
 app.get('/health', (req, res) => res.sendStatus(200));
 
 // Error handler

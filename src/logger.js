949ab7f79f41789e72340ba54611dc404056e7a2
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 05:26:59 -0300

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,14 +1,12 @@
 import { createLogger, format, transports } from 'winston';
 
 export const logger = createLogger({
-  level: process.env.LOG_LEVEL || 'info',
+  level: 'info',
   format: format.combine(
     format.timestamp(),
     format.printf(({ level, message, timestamp }) =>
       `${timestamp} [${level.toUpperCase()}] ${message}`
     )
   ),
-  transports: [
-    new transports.Console()
-  ]
+  transports: [ new transports.Console() ]
 });

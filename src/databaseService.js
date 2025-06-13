1c139897bb46977cb2fe179156355aa9e8c6dc1b
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 12:43:49 -0300

    Update databaseService.js

--- src/databaseService.js
+++ src/databaseService.js
@@ -1,5 +1,8 @@
-import pg from 'pg';
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// src/databaseService.js
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export async function query(text, params) {
   const res = await pool.query(text, params);

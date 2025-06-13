da88cb8fe66226ceb552fe3aad908dd30ee0df29
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 15:01:02 -0300

    Update webhooks.js

--- src/webhooks.js
+++ src/webhooks.js
@@ -1,30 +1,12 @@
-import express from 'express';
-import { parseSignal } from './signals.js';
-import { saveSignal } from './services/signalsService.js';
-import { saveDominance } from './services/coinstatsService.js';
-import { logger } from './logger.js';
-
-const router = express.Router();
-
-router.post('/signal', async (req, res, next) => {
-  try {
-    const sig = parseSignal(req.body);
-    await saveSignal(sig);
-    res.status(200).send('Signal received');
-  } catch (err) {
-    logger.error(err.stack || err);
-    next(err);
-  }
-});
+import { parseDominance, saveDominance } from './services/coinstatsService.js';
 
 router.post('/dominance', async (req, res, next) => {
   try {
-    await saveDominance(req.body);
+    const dom = parseDominance(req.body);
+    await saveDominance(dom);
     res.status(200).send('Dominance received');
   } catch (err) {
-    logger.error(err.stack || err);
+    logger.error(err.stack||err);
     next(err);
   }
 });
-
-export default router;

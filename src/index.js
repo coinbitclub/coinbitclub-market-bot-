6c885bb2ae8676ad44f0070f5eaa115d4ea18546
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 16:38:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-// src/index.js
-import express        from 'express';
-import dotenv         from 'dotenv';
-import webhookRoutes  from './webhooks.js';
+import express from 'express';
+import dotenv from 'dotenv';
+import webhookRoutes from './webhooks.js';
 import { setupScheduler } from './utils/scheduler.js';
-import { logger }     from './logger.js';
+import { logger } from './logger.js';
 
 dotenv.config();
 const app = express();
 app.use(express.json({ limit: '100kb' }));
 
-// ➜ Handler para raiz (health-check padrão)
 app.get('/', (_req, res) => res.send('OK'));
-
 app.use('/webhook', webhookRoutes);
 app.get('/health', (_req, res) => res.sendStatus(200));
-// …
 
 // error handler
 app.use((err, _req, res, _next) => {
-  logger.error(err.stack||err);
+  logger.error(err.stack || err);
   res.status(500).json({ error: 'Internal server error' });
 });
 

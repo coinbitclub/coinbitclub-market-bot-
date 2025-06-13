f038ed034888b13344256dacad3d4bdf41f27bbf
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 15:18:07 -0300

    Update webhooks.js

--- src/webhooks.js
+++ src/webhooks.js
@@ -1,12 +1,32 @@
+import express from 'express';
+import { parseSignal, saveSignal } from './services/signalsService.js';
 import { parseDominance, saveDominance } from './services/coinstatsService.js';
+import { logger } from './logger.js';
 
+const router = express.Router();
+
+// Sinais do TradingView
+router.post('/signal', async (req, res, next) => {
+  try {
+    const sig = parseSignal(req.body);
+    await saveSignal(sig);
+    res.status(200).send('Signal received');
+  } catch (err) {
+    logger.error(err.stack || err);
+    next(err);
+  }
+});
+
+// Dominância (BTC.D)
 router.post('/dominance', async (req, res, next) => {
   try {
     const dom = parseDominance(req.body);
     await saveDominance(dom);
     res.status(200).send('Dominance received');
   } catch (err) {
-    logger.error(err.stack||err);
+    logger.error(err.stack || err);
     next(err);
   }
 });
+
+export default router;

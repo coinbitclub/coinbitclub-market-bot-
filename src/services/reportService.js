daa8513d2d23462fd46054b9803b7febe4d68da2
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 11:57:24 -0300

    Create reportService.js

---
+++ src/services/reportService.js
@@ -0,0 +1,5 @@
+export async function sendReport(type) {
+  // type: 'asia_open', 'asia_close', 'us_macro'
+  console.log(`[Report] sendReport called for ${type}`);
+  // implementar geração/envio de e-mail ou mensagem aqui
+}

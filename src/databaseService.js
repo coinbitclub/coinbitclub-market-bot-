4c7ef45c46761bd2df304a80592d901d47b93e8f
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 05:54:15 -0300

    Update and rename src/services/databaseService to src/databaseService.js

---
+++ src/databaseService.js
@@ -0,0 +1,7 @@
+import pg from 'pg';
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+
+export async function query(text, params) {
+  const res = await pool.query(text, params);
+  return res;
+}

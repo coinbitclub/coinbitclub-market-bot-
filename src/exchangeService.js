011fe4cbda93925de1bff09f178eeb30eda2f4ef
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 00:28:47 -0300

    Create exchangeService.js

---
+++ src/exchangeService.js
@@ -0,0 +1,12 @@
+/* =============================================
+   src/exchangeService.js
+   ============================================= */
+export { placeBybitOrder } from './bybitService.js';
+export { placeBinanceOrder } from './binanceService.js';
+export async function fetchOpenPositions() {
+  // Implementar fetch em Bybit/Binance via API
+  return [];
+}
+export async function closePosition(id) {
+  // Implementar cancelamento/fechamento
+}

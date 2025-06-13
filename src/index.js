a1c75b02831c43725efd0e2306d4c10403575f78
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-13 11:54:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webhookRoutes from './webhooks.js';
-import apiRoutes from './webhooks.js';
 import { setupScheduler } from './utils/scheduler.js';
 import { logger } from './logger.js';
 
@@ -10,9 +9,9 @@ const app = express();
 app.use(express.json({ limit: '100kb' }));
 
 app.use('/webhook', webhookRoutes);
-app.use('/api', apiRoutes);
 app.get('/health', (req, res) => res.sendStatus(200));
 
+// error handler
 app.use((err, req, res, next) => {
   logger.error(err.stack || err);
   res.status(500).json({ error: 'Internal server error' });
@@ -24,5 +23,6 @@ const server = app.listen(PORT, () => {
   setupScheduler();
 });
 
+// graceful shutdown
 process.on('SIGTERM', () => server.close(() => logger.info('Server closed on SIGTERM')));
 process.on('SIGINT',  () => server.close(() => logger.info('Server closed on SIGINT')));

f57068e72c691e1aa8027877614fac51f0bebde2
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-14 00:16:45 -0300

    Update signalsService.js

--- src/services/signalsService.js
+++ src/services/signalsService.js
@@ -1,28 +1,24 @@
-import { query } from '../databaseService.js';
+import { pool } from './databaseService.js';
 
 export async function saveSignal(signal) {
-  const sql = `
-    INSERT INTO signals
-      (ticker, captured_at, close, ema9_30, rsi_4h, rsi_15, momentum_15,
-       atr_30, atr_pct_30, vol_30, vol_ma_30, diff_btc_ema7,
-       cruzou_acima_ema9, cruzou_abaixo_ema9, leverage)
-    VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)
+  const query = `
+    INSERT INTO signals (
+      ticker, close, ema9_30, rsi_4h, rsi_15, momentum_15,
+      atr_30, atr_pct_30, vol_30, vol_ma_30, diff_btc_ema7,
+      leverage, signal_time
+    ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)
   `;
-  await query(sql, [
-    signal.ticker,
-    signal.time,
-    signal.close,
-    signal.ema9_30,
-    signal.rsi_4h,
-    signal.rsi_15,
-    signal.momentum_15,
-    signal.atr_30,
-    signal.atr_pct_30,
-    signal.vol_30,
-    signal.vol_ma_30,
-    signal.diff_btc_ema7,
-    signal.cruzou_acima_ema9,
-    signal.cruzou_abaixo_ema9,
-    signal.leverage
-  ]);
+  const values = [
+    signal.ticker, signal.close, signal.ema9_30, signal.rsi_4h, signal.rsi_15,
+    signal.momentum_15, signal.atr_30, signal.atr_pct_30,
+    signal.vol_30, signal.vol_ma_30, signal.diff_btc_ema7,
+    signal.leverage, signal.signal_time
+  ];
+  try {
+    await pool.query(query, values);
+    console.log('Sinal salvo no banco:', signal.ticker, signal.signal_time);
+  } catch (err) {
+    console.error('Erro ao salvar sinal:', err);
+    throw err;
+  }
 }

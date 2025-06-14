ef8fd874a7f43671c1255eff6980b724685bdf22
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-14 00:11:00 -0300

    Update and rename logger to logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,10 @@
+// src/utils/logger.js
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});

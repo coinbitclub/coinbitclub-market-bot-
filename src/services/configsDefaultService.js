f8df8a8c7ac30170ec391f35cfbaee9567be6137
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-15 12:03:30 -0300

    Update configsDefaultService.js

--- src/services/configsDefaultService.js
+++ src/services/configsDefaultService.js
@@ -1,18 +1,20 @@
-// src/services/configsDefaultService.js
 import { executeQuery } from './databaseService.js';
 
 export async function getDefaultConfigs() {
-  const query = `SELECT * FROM configs_default WHERE ativa = true LIMIT 1;`;
+  const query = `
+    SELECT *
+    FROM configs_default
+    WHERE ativa = true
+    LIMIT 1;
+  `;
   return await executeQuery(query);
 }
 
 export async function saveDefaultConfigs(configs) {
-  // Exemplo: configs = { ... }
   const query = `
     INSERT INTO configs_default (/* campos */)
-    VALUES (/* valores */)
+    VALUES (/* valores derivados de configs */)
     RETURNING id;
   `;
-  // Ajuste conforme os campos da sua tabela
   return await executeQuery(query, [/* valores */]);
 }

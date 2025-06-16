346e3120d8ef0ae4bfbe7717dce58aa1968d10ee
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-16 03:40:39 -0300

    Update signalsService.js

--- src/services/signalsService.js
+++ src/services/signalsService.js
@@ -1,1 +1,16 @@
-import{executeQuery}from'./databaseService.js';import{logger}from'../logger.js';export async function saveSignal(s){const t=`INSERT INTO signals(ticker,time,close,ema9_30,rsi_4h,rsi_15,momentum_15,atr_30,atr_pct_30,vol_30,vol_ma_30)VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)`;await executeQuery(t,[s.ticker,s.time,s.close,s.ema9_30,s.rsi_4h,s.rsi_15,s.momentum_15,s.atr_30,s.atr_pct_30,s.vol_30,s.vol_ma_30]);logger.info('Signal saved',{ticker:s.ticker,time:s.time});}
+import pool from '../db.js';
+
+export async function saveSignal(data) {
+  // Adapte os campos conforme sua tabela signals
+  const {
+    ticker, time, close, ema9_30, rsi_4h, rsi_15,
+    momentum_15, atr_30, atr_pct_30, vol_30,
+    vol_ma_30, diff_btc_ema7, cruzou_acima_ema9, cruzou_abaixo_ema9
+  } = data;
+  await pool.query(
+    `INSERT INTO signals (
+      ticker, time, close, ema9_30, rsi_4h, rsi_15, momentum_15, atr_30, atr_pct_30, vol_30, vol_ma_30, diff_btc_ema7, cruzou_acima_ema9, cruzou_abaixo_ema9
+    ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)`,
+    [ticker, time, close, ema9_30, rsi_4h, rsi_15, momentum_15, atr_30, atr_pct_30, vol_30, vol_ma_30, diff_btc_ema7, cruzou_acima_ema9, cruzou_abaixo_ema9]
+  );
+}

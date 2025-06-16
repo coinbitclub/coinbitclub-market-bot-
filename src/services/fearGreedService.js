58c166b5a90d73062d0e904d220c46846c60bb97
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-16 03:49:10 -0300

    Create fearGreedService.js

---
+++ src/services/fearGreedService.js
@@ -0,0 +1,9 @@
+// src/services/fearGreedService.js
+
+// Stub temporário - remove quando implementar de verdade!
+export async function getFearGreedData() {
+  return {
+    value: 50,
+    timestamp: new Date().toISOString()
+  };
+}

34f5534f5d8e1f14141531ac4890f078eb34ba50
coinbitclub <faleconosco@coinbitclub.vip>
2025-06-16 19:38:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import express from 'express';
 import webhookRoutes from './routes/webhook.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para receber JSON!
 app.use(express.json());
+
+// Todas as rotas de webhook
 app.use('/webhook', webhookRoutes);
 
-const PORT = process.env.PORT || 3000;
+// Rota healthcheck (opcional)
+app.get('/', (req, res) => {
+  res.send('Server OK');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
